384c974de5a464f7be7778cfd520cd68caa03ded
sandig <sandi.gec@fri.uni-lj.si>
2023-12-28 15:14:46 +0100

    Prototype smart contracts

---
+++ access/ConcreteAccessControl.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.8.0) (access/AccessControl.sol)
+
+pragma solidity ^0.8.0;
+
+import "./AccessControl.sol";
+
+contract ConcreteAccessControl is AccessControl {
+    constructor() {
+    }
+}
